d66fddbe07b4b61ca489e53ae8edaf7231535c28
utsavmaniyar <utsavmaniyar@gmail.com>
2021-09-18 17:49:57 -0700

    problem(added): added DFS inorder traveral problem

---
+++ Problems/tree-traversal(LC-94).js
@@ -0,0 +1,57 @@
+/*
+PROBLEM STATEMENT
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+Example 1:
+
+        1
+          \
+           2
+          /
+        3   
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+==========================================================================
+
+Approch: This is a tree traversal and we have to display node values inorder. We will use recursive function.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+    if (!root) { return [] }
+    let results = [];
+    dfs(root, results);
+    return results
+};
+
+const dfs = (node, results) => {
+
+    if (!node.left && !node.right) {
+        results.push(node.val)
+        return
+    }
+
+    if (node.left) { dfs(node.left, results) }
+    results.push(node.val)
+    if (node.right) { dfs(node.right, results) }
+}
