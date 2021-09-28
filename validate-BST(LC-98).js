e276919a574cb68d06e293da8fa74ac5b3a11ef6
utsavmaniyar <utsavmaniyar@gmail.com>
2021-09-27 21:21:00 -0700

    problem(updated): validate BST

--- validate-BST(LC-98).js
+++ validate-BST(LC-98).js
@@ -16,11 +16,10 @@ Input: root = [5,1,4,null,null,3,6]
 Output: false
 Explanation: The root node's value is 5 but its right child's value is 4.
 ======================================================================================================
-Approach: we have to traverse each node and add the node value in lessThan and GreatherThan Array to check all child nodes.
-while visiting child node we have to compare the value with lt and gt array and mark isBST variable to false.
+Approach: we will perform inorder traversal and create the array. once it done, we will check each elemt to it's next element.
+if privious element is grather than the next one, we will return false.
 
 */
-
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -33,45 +32,30 @@ while visiting child node we have to compare the value with lt and gt array and 
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function (root) {
-    if (!root) {
+ var isValidBST = function(root) {
+    if(!root){
         return false
     }
-    let result = { isBST: true }
-    helper(root, result, [], []);
-    return result.isBST;
+    let treeArray = []
+    helper(root, treeArray);
+    for(let i=0; i < treeArray.length; i++){
+        if(treeArray[i] >= treeArray[i+1]){
+            return false;
+        }
+    }
+    return true;
 };
 
-const helper = (node, result, lt, gt) => {
-    lt.some(val => {
-        if (val <= node.val) {
-            result.isBST = false;
-            return
-        }
-    })
-
-    gt.some(val => {
-        if (val >= node.val) {
-            result.isBST = false;
-            return
-        }
-    })
-
-    if (!node.left && !node.right) {
+const helper = (node, treeArray) => {     
+    if(!node.left && !node.right){
+        treeArray.push(node.val)
         return
     }
-
-    lt.push(node.val)
-    if (node.left) {
-        if (node.val <= node.left.val) {
-            result.isBST = false
-        }
-        helper(node.left, result, lt, gt)
+    if(node.left){
+        helper(node.left, treeArray)
     }
-    lt.pop()
-    gt.push(node.val)
-    if (node.right) {
-        helper(node.right, result, lt, gt)
+    treeArray.push(node.val)
+    if(node.right){
+        helper(node.right, treeArray)
     }
-    gt.pop()
 }
